1cec9a93badbc78cc6d1e485ae021f6c30e5238f
Makarov96 <estev97@gmail.com>
2022-05-04 20:41:02 -0600

    Initial Commit

---
+++ Shared/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  Shared
+//
+//  Created by Guerin Steven Colocho Chacon on 4/05/22.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
